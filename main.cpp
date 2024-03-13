536e41fcca0fb610a77709fa247638cfc2851c59
Sergio Parejo <serparram@alum.us.es>
2024-03-13 15:30:14 +0100

    Esto es una prueba sobre lo que hare

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+// Estoy haciendo preubas
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
