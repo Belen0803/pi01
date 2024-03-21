a96b9c263cf2b0d1293aea2165966e613857eb43
Sergio Parejo <serparram@alum.us.es>
2024-03-21 11:47:04 +0100

    Añadido el modo de light sleep

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,30 @@
 #include<Arduino.h>
 
+#define THRESHOLD 50
+
+int cont = 0;
+
+// Use of ISRAM memory, faster than FLASH
+void IRAM_ATTR touchSensorWakeUp(){
+    Serial.println(cont);
+    cont++;
+}
+
 void setup() 
 {
     Serial.begin(115200);
-    delay(5000);
+    // Monitor Serie use
+    delay(1000);
+    // Disbaling all options except WIFI and touchpad
+    esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_ALL);
+    esp_sleep_enable_wifi_wakeup();
+    esp_sleep_enable_touchpad_wakeup();
+
+    // Use a threshold for the pin to wake up
+    // the function is used as an interruption to use it to do the first things we use
+    touchAttachInterrupt(T9,touchSensorWakeUp,THRESHOLD);
 }
-
-int counter = 0;
-
+ 
 void loop() {
-  Serial.println(counter);
-  counter ++;
-
-  esp_sleep_enable_timer_wakeup(2 * 1000000); //light sleep durante 2 segundos
-  esp_light_sleep_start();  
+  esp_light_sleep_start();
 }
