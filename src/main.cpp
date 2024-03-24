3a95d6730eac370135d40b095e679446b51c34b0
Sergio Parejo <serparram@alum.us.es>
2024-03-24 16:45:28 +0100

    Uso del termometro Tambien he cambiado el nombre de una libreria por errores en el codigo

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include<Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+//Defines
 #define THRESHOLD 60
 
-const int ENCENDIDO = T9; // GPIO32
+// Defined pins
+const int encendido = T6; // GPIO33, touch sensor, on/off button
+const int thermoBus = GPIO_NUM_25; // GPIO36(UP), temperature bus
 
+// Usage of thermometer buses
+OneWire oneWire(thermoBus);
+DallasTemperature sensors (&oneWire);
+
+// Variables
 int cont = 0;
+float temperatureC;
 
-// Use of ISRAM memory, faster than FLASH
+// Interruptions
+// Capacitor sensor wake up. Use of ISRAM memory, faster than FLASH
 void IRAM_ATTR touchSensorWakeUp(){
     Serial.println(cont);
     cont++;
@@ -15,7 +27,7 @@ void IRAM_ATTR touchSensorWakeUp(){
 void setup() 
 {
     Serial.begin(115200);
-    // Monitor Serie use
+    sensors.begin();
     delay(1000);
     // Disbaling all options except WIFI and touchpad
     esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_ALL);
@@ -24,10 +36,14 @@ void setup()
 
     // Use a threshold for the pin to wake up
     // the function is used as an interruption to use it to do the first things we use
-    touchAttachInterrupt(ENCENDIDO,touchSensorWakeUp,THRESHOLD);
+    touchAttachInterrupt(encendido,touchSensorWakeUp,THRESHOLD);
 }
  
 void loop() {
   esp_light_sleep_start();
-  delay(1000);
+  // Ask thermometer for temperature, save it and print it
+  sensors.requestTemperatures();
+  temperatureC = sensors.getTempCByIndex(0);
+  Serial.println(temperatureC);
+  delay(3000);
 }
