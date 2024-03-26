eb4e6c852e57ce3296f862d06ada513a4cd2c3da
Sergio Parejo <serparram@alum.us.es>
2024-03-26 10:57:46 +0100

    Adición de cómo se transmitiran los datos

--- Controlador/src/main.cpp
+++ Controlador/src/main.cpp
@@ -21,8 +21,8 @@ HardwareSerial SerialPort(2);
 
 // Variables
 int cont = 0;
-float temperatureC;
-int comunicaciones;
+float tempM;
+int bebida, water, sugar, tank1, tank2,tank3,tank4,tempI;
 bool botonApagar = 0;
 
 // Interruptions
@@ -50,12 +50,42 @@ void setup()
 void loop() {
   esp_light_sleep_start();
   // Ask thermometer for temperature, save it and print it
-  sensors.requestTemperatures();
-  temperatureC = sensors.getTempCByIndex(0);
-  Serial.println(temperatureC);
+  /*sensors.requestTemperatures();
+  tempM = sensors.getTempCByIndex(0);
+  Serial.println(tempM);*/
+
+  // There will be only one comunicattion, each number will correspond to one characteristic
+  // of the drink, except for temperature, will be 2 digits for it
   if (SerialPort.available()){
-    comunicaciones = SerialPort.read();
+    bebida = SerialPort.read();
+    SerialPort.print(200);
+
+    // We get every number and its corresponding value
+    // 1º number -> Sugar
+    sugar = bebida % 10;
+    bebida = bebida / 10;
+    // 2º number -> Water
+    water = bebida % 10;
+    bebida = bebida / 10;
+    // 3º number -> Tank 1
+    tank1 = bebida % 10;
+    bebida = bebida / 10;
+    // 4º number -> Tank 2
+    tank2 = bebida % 10;
+    bebida = bebida / 10;
+    // 5º number -> Tank 3
+    tank3 = bebida % 10;
+    bebida = bebida / 10;
+    // 6º number -> Tank 4
+    tank4 = bebida % 10;
+    bebida = bebida / 10;
+    // 7º & 8º number -> Temperature
+    tempI = bebida;
   }
+
+
+
+
   // Information will be transmited from esp WIFI each number will be referred to each tank
   // There will be an exception in case the turn off button is pressed so the variable will be saved
   
